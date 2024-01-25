372c0839e17b705d1dfc66eac1d249dbc10b61ab
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2024-01-25 18:04:28 -0500

    RTK Query

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { Provider } from 'react-redux'
-import { store } from './store/store.js'
-import { PokemonApp } from './PokemonApp.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./store/store.js";
+import { PokemonApp } from "./PokemonApp.jsx";
+import { TodoApp } from "./TodoApp.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-    {/* <App /> */}
-    <PokemonApp></PokemonApp>
+      {/* <App /> */}
+      {/* <PokemonApp></PokemonApp> */}
+      <TodoApp></TodoApp>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
